5f7c46820bf323c97823fee5234d18e0630ed3d8
Nikolaj Fedulov <v20nickf1@gmail.com>
2022-07-27 19:45:52 +0300

    Initial commit for listing 01.02

---
+++ src/main/java/listing_01_02/HelloWorld.java
@@ -0,0 +1,20 @@
+package listing_01_02;
+
+/**
+ * Explanation of the content and features of the program...
+ * @author Nikolaj Fedulov
+ * @version 1.0
+ */
+public class HelloWorld{                        // HelloWorld is just a name
+
+    // The following method starts the program execution
+    public static void main(String[] args){     // args are not used
+
+        /* The following method simply prints its argument to the display screen */
+        System.out.println("Hello, XXI Century World!");
+
+        // The following call is commented out,
+        // method will not run
+//        System.out.println("Farewell, XX Century!");
+    }
+}
